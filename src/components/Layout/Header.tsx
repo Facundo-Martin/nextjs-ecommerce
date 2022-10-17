3e4f7d93e556150702af6af3939a955b827caf20
Facundo Martin <facundomartin98@gmail.com>
2022-10-17 09:51:37 -0700

    added nav and installed tailwindcss prettier plugin

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -4,23 +4,36 @@ import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 export default function Header() {
   return (
     <header>
-      <div className="flex items-center bg-[#131921] p-1 flex-grow py-2">
-        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
+      <div className="flex flex-grow items-center gap-x-6 bg-[#131921] p-1 py-2">
+        <div className="mt-2 flex flex-grow items-center sm:flex-grow-0">
           <Image
             src="https://links.papareact.com/f90"
-            width={150}
+            width={140}
             height={40}
             objectFit="contain"
             className="cursor-pointer"
           />
         </div>
-        <div className="hidden sm:flex flex-grow cursor-pointer items-center h-10 rounded-md bg-yellow-400 hover:bg-yellow-500">
+        <div className="hidden h-10 flex-grow cursor-pointer items-center rounded-md bg-yellow-400 hover:bg-yellow-500 sm:flex">
           <input
-            className="flex-1 h-full rounded-l-md focus:outline-none px-4"
+            className="h-full flex-1 rounded-l-md px-4 focus:outline-none"
             type="text"
           />
           <MagnifyingGlassIcon className="h-12 p-4" />
         </div>
+        <nav className="flex items-center gap-x-4 whitespace-nowrap text-center text-xs text-white">
+          <div>
+            <p>Hello User</p>
+            <p>Account & Lists</p>
+          </div>
+          <div>
+            <p>Returns</p>
+            <p>& Orders</p>
+          </div>
+          <div>
+            <p>Cart</p>
+          </div>
+        </nav>
       </div>
     </header>
   );
