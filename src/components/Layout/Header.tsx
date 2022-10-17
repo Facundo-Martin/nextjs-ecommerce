5348c8e7e4491a46701c59c1243548ce60772c29
Facundo Martin <facundomartin98@gmail.com>
2022-10-17 09:06:47 -0700

    added logo

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -4,12 +4,14 @@ import React from "react";
 export default function Header() {
   return (
     <header>
-      <div>
-        <div>
+      <div className="flex items-center bg-[#131921] p-1 flex-grow py-2">
+        <div className="mt-2 flex items-center flex-grow sm:flex-grow-0">
           <Image
             src="https://links.papareact.com/f90"
             width={150}
             height={40}
+            objectFit="contain"
+            className="cursor-pointer"
           />
         </div>
       </div>
