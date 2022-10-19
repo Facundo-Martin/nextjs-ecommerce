15e7d5586418f0a2be42e20aad2a773fd8948f7a
Facundo Martin <facundomartin98@gmail.com>
2022-10-19 10:35:04 -0700

    finished ProductCard base layout

--- src/components/Home/ProductCard.tsx
+++ src/components/Home/ProductCard.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { StarIcon } from "@heroicons/react/24/solid";
+import Image from "next/image";
+import React, { useState } from "react";
 import { Product } from "../../interfaces";
 
 export default function ProductCard({
@@ -8,11 +10,41 @@ export default function ProductCard({
   description,
   category,
   image,
-  rating,
 }: Product) {
+  const maxRating = 5;
+  const minRating = 1;
+  const productRating = Math.floor(
+    Math.random() * (maxRating - minRating + 1) + minRating
+  );
+  const hasPrime = Math.random() < 0.5;
   return (
     <div className="p-4">
       <p>Category</p>
+      <Image src={image} height={200} width={200} objectFit="contain" />
+      <h4>{title}</h4>
+      <div className="flex items-center">
+        {Array(productRating)
+          .fill(1)
+          .map((i) => (
+            <StarIcon key={i} className="w-5 fill-yellow-400" />
+          ))}
+      </div>
+
+      <p>{description}</p>
+      <div>{price}</div>
+      {hasPrime && (
+        <>
+          <Image
+            src="https://links.papareact.com/fdw"
+            alt="Prime"
+            width={40}
+            height={40}
+            objectFit="contain"
+          />
+          <p>Free next-day delivery</p>
+        </>
+      )}
+      <button>Add to basket</button>
     </div>
   );
 }
